c220ef3518063795df38219ef5e3a2a72bb18660
Piotrek <pjastrzebscy@gmail.com>
2019-03-20 00:47:23 +0100

    build and change of netlify.toml file for deployment

---
+++ build/precache-manifest.617ef3d28c28140f3a22dc7799345773.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aad36fa2643d68752180",
+    "url": "/static/js/main.aad36fa2.chunk.js"
+  },
+  {
+    "revision": "6bc4caf456162f70a2d0",
+    "url": "/static/js/1.6bc4caf4.chunk.js"
+  },
+  {
+    "revision": "aad36fa2643d68752180",
+    "url": "/static/css/main.0c95e775.chunk.css"
+  },
+  {
+    "revision": "05c8ebca86073cda34b2c8cad95de677",
+    "url": "/index.html"
+  }
+];
